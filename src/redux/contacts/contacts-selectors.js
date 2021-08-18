393f588c8c0ab1e7deb6a4562435b8352ddcedd5
OlhaNikoliuk <nikoliukolha@gmail.com>
2021-08-18 14:16:25 +0300

    add memoization

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,14 +1,16 @@
+import {createSelector} from '@reduxjs/toolkit';
 export const getContactList = (state) => state.contacts.items;
 export const getFilterValue = (state) => state.contacts.filter;
 export const isLoadingValue = (state) => state.contacts.isLoading;
 
-export const getVisibleContacts = (state) => {
-  const contactList = getContactList(state);
-  const filterValue = getFilterValue(state);
+export const getVisibleContacts = createSelector(
+  [getContactList, getFilterValue],
 
-  const normalizedFilter = filterValue.toLowerCase();
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
 
-  return contactList
-    .filter(({ name }) => name.toLowerCase().includes(normalizedFilter))
-    .sort((a, b) => a.name.localeCompare(b.name));
-};
+    return contacts
+      .filter(({ name }) => name.toLowerCase().includes(normalizedFilter))
+      .sort((a, b) => a.name.localeCompare(b.name));
+  }
+);
